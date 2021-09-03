3f5ec1df1d8b12f5b30ffe2ec1ec3df20f8011f7
Nick Allen <rutherford.t.b@gmail.com>
2021-09-03 15:53:11 -0500

    find diff with 2 arrays works with test case

--- testSymmetric/testSym.js
+++ testSymmetric/testSym.js
@@ -7,7 +7,13 @@ const secArr = [2, 3, 4];
 const sim = [];
 const diff = [];
 
+function testFull(f, s){
+  testPart(f,s);
+  testPart(s,f);
+}
+
 function testPart(first, second){
+  console.log('testPart runs...');
   for(x of first){
     let match = false;
 
@@ -15,34 +21,23 @@ function testPart(first, second){
     for(y of second){
       if(x === y){
         match = true;
+        console.log('matched ', x);
         break;
       }
     }
-
     //if there was no match in second array
     //and the unmatched element doesn't already exist in the diff array
     //push to diff
-    if(!match && diff.indexOf(x) !== -1){
+
+    if(!match && diff.indexOf(x) === -1){
+      console.log('pushing...');
       diff.push(x);
     }
   }
 }
 
+//run the test!
+testFull(firstArr, secArr);
 
 console.log('sim: ', sim);
 console.log('diff: ', diff);
-console.log('firstArr before:', firstArr);
-
-
-for(el of sim){
-  firstArr.splice(firstArr.indexOf(el));
-}
-
-console.log('firstArr after: ', firstArr);
-
-
-
-
-
-console.log(firstArr);
-console.log(secArr);
