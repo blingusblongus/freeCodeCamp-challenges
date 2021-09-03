f271b48fb11f5318da8cdf896ac5e0ba13f4e0f1
Nick Allen <rutherford.t.b@gmail.com>
2021-09-03 15:45:44 -0500

    added diff push and wrapped in function

--- testSymmetric/testSym.js
+++ testSymmetric/testSym.js
@@ -4,19 +4,45 @@
 // working on finding the symmetric difference between just two:—
 const firstArr = [1, 2, 3];
 const secArr = [2, 3, 4];
+const sim = [];
 const diff = [];
 
-for(x of firstArr){
-  for(y of secArr){
-    if(x === y){
+function testPart(first, second){
+  for(x of first){
+    let match = false;
+
+    //check for any matches in second array
+    for(y of second){
+      if(x === y){
+        match = true;
+        break;
+      }
+    }
+
+    //if there was no match in second array
+    //and the unmatched element doesn't already exist in the diff array
+    //push to diff
+    if(!match && diff.indexOf(x) !== -1){
       diff.push(x);
     }
   }
 }
 
 
+console.log('sim: ', sim);
+console.log('diff: ', diff);
+console.log('firstArr before:', firstArr);
 
-console.log(diff);
+
+for(el of sim){
+  firstArr.splice(firstArr.indexOf(el));
+}
+
+console.log('firstArr after: ', firstArr);
+
+
+
+
 
 console.log(firstArr);
 console.log(secArr);
